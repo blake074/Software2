0b142aaf6fcf513e1e3c795944f1b8ba8e738de6
blake074 <salbanl@unbosque.edu.co>
2024-03-21 12:30:06 -0500

    ???? no encuentra los mappings

--- EmpresaABC/src/main/java/co/edu/unbosque/EmpresaAbcApplication.java
+++ EmpresaABC/src/main/java/co/edu/unbosque/EmpresaAbcApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("co.edu.unbosque.model.database")
 public class EmpresaAbcApplication {
 
     public static void main(String[] args) {
