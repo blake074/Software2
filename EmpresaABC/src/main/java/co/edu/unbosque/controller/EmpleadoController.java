30825a5cd86d246fcaeef51e6fc356a03ca3794f
wleal797 <leal.wilson19@gmail.com>
2024-03-22 01:48:11 -0500

    Frontend y backend

--- EmpresaABC/src/main/java/co/edu/unbosque/controller/EmpleadoController.java
+++ EmpresaABC/src/main/java/co/edu/unbosque/controller/EmpleadoController.java
@@ -1,77 +1,21 @@
 package co.edu.unbosque.controller;
 
-import co.edu.unbosque.model.entities.Empleado;
-import co.edu.unbosque.model.entities.EmpleadoDTO;
-import co.edu.unbosque.model.repositories.EmpleadoRepository;
-import co.edu.unbosque.model.services.EmpleadoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
-
-/**
- * El controlador de la entidad Empleado, que maneja las solicitudes relacionadas con los empleados.
- */
-@RestController
-@RequestMapping("/api/empleados")
+@Controller
 public class EmpleadoController {
 
-    /**
-     * El servicio de empleado que maneja la lógica de negocio relacionada con los empleados.
-     */
-    @Autowired
-    private EmpleadoService empleadoService;
-
-    /**
-     * El repositorio de empleado que proporciona acceso a la base de datos para los empleados.
-     */
-    @Autowired
-    private EmpleadoRepository empleadoRepository;
-
-    /*
-    @GetMapping("/lista")
-    public List<Empleado> obtenerEmpleados() {
-        return empleadoRepository.findAll();
+    @GetMapping ("/")
+    public ModelAndView index(){
+        System.out.println("11036576");
+        return new ModelAndView("Proyecto");
     }
-     */
-
-    /**
-     * Maneja las solicitudes GET para obtener una lista de empleados.
-     * @return Una lista de objetos EmpleadoDTO que representan los empleados.
-     */
-    @GetMapping("/lista")
-    public List<EmpleadoDTO> obtenerEmpleados() {
-        return (List<EmpleadoDTO>) empleadoService.obtenerEmpleados();
-    }
-
-
-    /**
-     * Maneja las solicitudes POST para crear un nuevo empleado.
-     * @param empleado El objeto Empleado recibido en el cuerpo de la solicitud.
-     * @return El objeto EmpleadoDTO creado para el empleado.
-     */
-    @PostMapping("/crear")
-    public EmpleadoDTO crearEmpleado(@RequestBody Empleado empleado) {
-        return empleadoService.crearEmpleado(empleado);
-    }
-
-    /**
-     * Maneja las solicitudes PUT para actualizar un empleado existente.
-     * @param empleado El objeto Empleado recibido en el cuerpo de la solicitud.
-     * @return El objeto EmpleadoDTO actualizado para el empleado.
-     */
-    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public EmpleadoDTO actualizarEmpleado(@RequestBody Empleado empleado) {
-        return empleadoService.crearEmpleado(empleado);
-    }
-
-    /**
-     * Maneja las solicitudes DELETE para eliminar un empleado por su ID.
-     * @param id_empleado El ID del empleado que se eliminará.
-     */
-    @DeleteMapping("{id_empleado}")
-    public void eliminarEmpleado(@PathVariable("id_empleado") int id_empleado) {
-        empleadoService.eliminarEmpleados(id_empleado);
+    @GetMapping ("/ProyectoForm")
+    public ModelAndView proyectosForm(){
+         System.out.println("118332023");
+        return new ModelAndView("ProyectoForm");
     }
 }
